d337f5eb5097b5c39b6ac346e62a7962361f39b2
cgp-client <code.gen.pro.system@gmail.com>
2024-09-26 14:34:02 +0300

    Update controllers/chat.js

--- controllers/chat.js
+++ controllers/chat.js
@@ -1,16 +1,10 @@
 const express = require('express');
 const { setupWebSocket } = require('./services/chatService');
 
-const app = express();
-
 const chatRouter = express.Router();
 
 chatRouter.get('/messages', (req, res) => {
   res.send([]);
 });
 
-app.use('/chat', chatRouter);
-
-setupWebSocket(app);
-
-module.exports = { chatRouter };
+module.exports = chatRouter;
